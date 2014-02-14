bec9fc04e9eb37baacaf5c7c632c92f6f2cddeb6
yejune <a@apptiz.com>
2014-02-14 17:10:28 +0900

    implode시 `,`가 잘못들어감

--- router.php
+++ router.php
@@ -71,7 +71,7 @@ class router
 	/*domain에 있을 경우 access세팅*/
 	public function setAccessByDomain($mode, $default = false)
 	{
-		if(preg_match('#((?P<access>'.implode(',|',$mode).')\.)#',$_SERVER['HTTP_HOST'], $m) 
+		if(preg_match('#((?P<access>'.implode('|',$mode).')\.)#',$_SERVER['HTTP_HOST'], $m) 
 			&& isset($m['access'])
 		) {
 			$this->setDefaultAccess($m['access']);
